98843148870a07b6a9e45e30977a6eec4bb6b836
David von Knobelsdorff <david.vonknobelsdorff@dm.de>
2021-10-05 14:32:42 +0200

    add default implementation with empty completion handler

--- Atlas/Atlas/Coordinators/MVVMCAppCoordinator.swift
+++ Atlas/Atlas/Coordinators/MVVMCAppCoordinator.swift
@@ -2,3 +2,10 @@ public protocol MVVMCAppCoordinator: AnyObject {
     func start()
     func deepLink(chain: [MVVMCNavigationRequest], selectedTab: Int?, completion: (() -> Void)?)
 }
+
+public extension MVVMCAppCoordinator {
+    func deepLink(chain: [MVVMCNavigationRequest], selectedTab: Int?) {
+        guard let selectedTab = selectedTab else { return }
+        deepLink(chain: chain, selectedTab: selectedTab, completion: nil)
+    }
+}
